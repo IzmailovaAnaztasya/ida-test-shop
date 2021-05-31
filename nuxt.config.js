5a722db3095151499be91e73a5806714d36ab079
IzmailovaAnaztasya <izmailova.anastasiy@yandex.ru>
2021-05-31 20:02:15 +0300

    change nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+  router: {
+    base: '/ida-test-shop/'
+  },
   head: {
     title: 'ida-test-shop',
     htmlAttrs: {
